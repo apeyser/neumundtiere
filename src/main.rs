f952fbb87eb1d242fe422421e5c980471600274d
Alexander Peyser <peyser.alex@gmail.com>
2024-06-16 19:17:27 +0200

    rpn: running

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,17 @@ mod rpn;
 use rpn::*;
 
 fn main() {
-    let mut vm = Vm::new();
-    let v: Vec<Frame> = vec![3.into(), 1.into(), ADD.into(), NEG.into()];
-    match vm.exec(v) {
+    match Vm::new().exec(
+        vec![
+            3.into(),
+            1.into(),
+            ADD.into(),
+            NEG.into(),
+            2.into(),
+            SUB.into(),
+            NEG.into(),
+        ]
+    ) {
         Ok(Some(f)) => println!("Result: {}", f),
         Ok(None) => println!("Empty stack"),
         Err(e) => println!("Error: {:?}", e)
