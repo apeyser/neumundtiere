b4b79a79ea3c7d8245f8a3762e9401b79a6c473e
Alexander Peyser <peyser.alex@gmail.com>
2024-06-22 13:56:01 +0200

    exec_line: return bool

--- src/main.rs
+++ src/main.rs
@@ -32,28 +32,28 @@ impl Iterator for Lines {
     }
 }
 
-fn exec_line(vm: &mut Vm, reader: &Reader, string: &str) -> Option<()> {
+fn exec_line(vm: &mut Vm, reader: &Reader, string: &str) -> bool {
     match reader.parse(string) {
         Err(err) => println!("Parse error: {err:?}"),
         Ok(frames) => {
             match vm.exec(frames) {
                 Err(rpn::Error::Quit) => {
-                    println!("Quiting with stack {:?}", vm.stack());
-                    return None
+                    println!("Quitting with stack {:?}", vm.stack());
+                    return false
                 },
                 Err(err) => println!("Exec error: {err:?}, {:?}", vm.stack()),
                 Ok(_) => (),
             }
         }
     };
-    Some(())
+    true
 }
 
 fn main() {
     let mut vm = Vm::new();
     let reader = Reader::new();
     for line in Lines::new() {
-        if None == exec_line(&mut vm, &reader, &line) {
+        if ! exec_line(&mut vm, &reader, &line) {
             break
         }
     }
