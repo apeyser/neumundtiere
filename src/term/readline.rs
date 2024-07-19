b0b88d7aafa6f52ccc0880df6b0fd1a5ffc3820d
Alexander Peyser <peyser.alex@gmail.com>
2024-07-19 21:46:18 +0200

    Add DEUTEROSTOME_HISTORY, DEUTEROSTOME_HISTORY_DIR

--- src/term/readline.rs
+++ src/term/readline.rs
@@ -1,8 +1,8 @@
 use std::io;
 use std::io::Write;
 use std::ops::Drop;
-use std::path::Path;
-use std::env::current_dir;
+use std::path::{Path, PathBuf};
+use std::env::{var, current_dir};
 
 use rustyline::error::ReadlineError;
 use rustyline::{self, DefaultEditor};
@@ -25,19 +25,30 @@ impl Lines {
     #[cfg(feature = "with-file-history")]
     fn build(mut editor: rustyline::DefaultEditor) -> rustyline::Result<Self> {
         const HISTFILE: Option<&'static str> = option_env!("DEUTEROSTOME_HISTORY");
-        let histfile = HISTFILE.unwrap_or(".deuterostome-history");
+        const HISTFILE_DIR: Option<&'static str> = option_env!("DEUTEROSTOME_HISTORY_DIR");
+        
+        let histfile = var("DEUTEROSTOME_HISTORY")
+            .unwrap_or_else(|_| HISTFILE.unwrap_or(".deuterostome-history").to_string());
+        let histfile_dir = var("DEUTEROSTOME_HISTORY_DIR")
+            .unwrap_or_else(|_| HISTFILE_DIR.unwrap_or("~").to_string());
+        
         let save = {
-            let mut path_buf = match home_dir() {
-                None => current_dir()?,
-                Some(path_buf) => path_buf,
+            let mut path_buf = if histfile_dir != "~" {
+                PathBuf::from(histfile_dir)
+            } else {
+                match home_dir() {
+                    None => current_dir()?,
+                    Some(path_buf) => path_buf,
+                }
             };
             path_buf.push(histfile);
             path_buf
         }.into_boxed_path();
-            
+
+        let path = save.display();
+        eprintln!("Using {path} for history");
         if let Err(err) = editor.load_history(&*save) {
-            let path = save.display();
-            println!("Unable to read save file {path}: {err}");
+            eprintln!("Unable to read save file {path}: {err}");
         };
         Ok(Self {save, editor})
     }
@@ -55,7 +66,7 @@ impl Drop for Lines {
             let Self {save, editor} = self;
             if let Err(err) = editor.save_history(&(*save)) {
                 let save = save.display();
-                println!("Unable write save file {save}: {err}")
+                eprintln!("Unable write save file {save}: {err}")
             }
         }
     }
@@ -70,15 +81,15 @@ impl Iterator for Lines {
         match editor.readline(">> ") {
             Ok(line) => {
                 if let Err(err) = editor.add_history_entry(line.as_str()) {
-                    println!("Readline error: {err}")
+                    eprintln!("Readline error: {err}")
                 };
                 Some(line)
             },
             Err(error) => {
                 match error {
-                    ReadlineError::Interrupted => println!("CTRL-C"),
-                    ReadlineError::Eof => println!("CTRL-D"),
-                    err => println!("Error: {:?}", err),
+                    ReadlineError::Interrupted => eprintln!("CTRL-C"),
+                    ReadlineError::Eof => eprintln!("CTRL-D"),
+                    err => eprintln!("Error: {:?}", err),
                 };
                 None
             }
@@ -90,7 +101,7 @@ pub fn exec(reader: &mut Reader) -> MainResult {
     match Lines::new() {
         Ok(lines) => super::exec(reader, lines),
         Err(err) => {
-            println!("Readline error: {err}");
+            eprintln!("Readline error: {err}");
             Err(Box::new(err))
         }
     }
